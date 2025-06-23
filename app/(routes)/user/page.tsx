89eeae5239895abeb82257768c90a51ed1e69563
David Lam <dlampatricio@gmail.com>
2025-06-23 10:09:48 -0400

    fix: no user message

--- app/(routes)/user/page.tsx
+++ app/(routes)/user/page.tsx
@@ -1,5 +1,17 @@
+"use client"
+
 export default function Page() {
+
   return (
-    <p>Panel de Usuario</p>
+    <div className="max-w-4xl py-4 mx-auto sm:py-32 sm:px-24">
+      <h1 className="text-3xl sm:text-2xl">
+        Panel de Usuario
+      </h1>
+      <div>
+        <div>
+            <p>No hay información del usuario.</p>
+        </div>
+      </div>
+    </div>
   )
 }
