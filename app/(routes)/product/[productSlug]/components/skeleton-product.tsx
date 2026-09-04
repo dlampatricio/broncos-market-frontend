2115fcc307366469b4c74838c1bf176f70e1d346
dlampatricio <dlampatricio@gmail.com>
2026-09-03 23:49:27 -0400

    chore: update skeleton components styling

--- app/(routes)/product/[productSlug]/components/skeleton-product.tsx
+++ app/(routes)/product/[productSlug]/components/skeleton-product.tsx
@@ -1,26 +1,26 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import { Skeleton } from '@/components/ui/skeleton';
 
 const SkeletonProduct = () => {
-  return ( 
+  return (
     <div className="max-w-6xl py-4 mx-auto sm:py-32 sm:px-24">
       <div className="grid gap-6 px-4 sm:grid-cols-2 sm:px-0">
         {/* Imagen */}
         <div className="flex justify-center sm:block">
-          <Skeleton className="h-[300px] w-full rounded-lg sm:h-[200px] sm:w-[350px]" />
+          <Skeleton className="h-[300px] w-full rounded-t-lg sm:h-[200px] sm:w-[350px]" />
         </div>
-        
+
         {/* Info del producto */}
         <div className="space-y-4">
           <div className="flex justify-between">
             <Skeleton className="h-8 w-[200px]" />
           </div>
-          
+
           <Skeleton className="h-4 w-full" />
           <Skeleton className="h-4 w-[80%]" />
           <Skeleton className="h-4 w-[60%]" />
-          
+
           <Skeleton className="h-6 w-[120px] my-4" />
-          
+
           <div className="flex items-center gap-4 pt-4">
             <Skeleton className="h-10 flex-grow" />
             <Skeleton className="h-10 w-10" />
@@ -29,6 +29,6 @@ const SkeletonProduct = () => {
       </div>
     </div>
   );
-}
- 
+};
+
 export default SkeletonProduct;
--- components/skeleton-schema.tsx
+++ components/skeleton-schema.tsx
@@ -1,39 +1,35 @@
-import { Skeleton } from "./ui/skeleton";
+import { Skeleton } from './ui/skeleton';
 
 type SkeletonSchemaProps = {
   grid: number;
   variant?: 'product' | 'category';
-}
+};
 
 const SkeletonSchema = (props: SkeletonSchemaProps) => {
-  const { grid, variant = 'product' } = props; 
+  const { grid, variant = 'product' } = props;
 
   if (variant === 'category') {
-    return (
-      Array.from({length: grid}).map((_, index) => (
-        <div key={index} className="relative group overflow-hidden rounded-lg">
-          <Skeleton className="w-full h-[260px] sm:h-[600px] rounded-lg"/>
-        </div>
-      ))
-    );
+    return Array.from({ length: grid }).map((_, index) => (
+      <div key={index} className="relative group overflow-hidden rounded-lg">
+        <Skeleton className="w-full h-[260px] sm:h-[600px] rounded-lg" />
+      </div>
+    ));
   }
 
   // Variante para productos
-  return ( 
-    Array.from({length: grid}).map((_, index) => (
-      <div key={index}>
-        <div className="group relative block overflow-hidden rounded-lg border border-border">
-          <Skeleton className="w-full aspect-square rounded-lg"/>
-          <div className="p-4 space-y-2">
-            <div className="flex justify-between gap-6">
-              <Skeleton className="h-5 w-3/4"/>
-              <Skeleton className="h-5 w-16 rounded-full"/>
-            </div>
+  return Array.from({ length: grid }).map((_, index) => (
+    <div key={index}>
+      <div className="group relative block overflow-hidden rounded-lg border border-border">
+        <Skeleton className="w-full aspect-square rounded-t-lg" />
+        <div className="p-4 space-y-2">
+          <div className="flex justify-between gap-6">
+            <Skeleton className="h-5 w-3/4" />
+            <Skeleton className="h-5 w-16 rounded-full" />
           </div>
         </div>
       </div>
-    ))
-  );
-}
- 
+    </div>
+  ));
+};
+
 export default SkeletonSchema;
